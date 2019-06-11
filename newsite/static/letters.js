66d16a5b9879cbe68d47be471335d645bad651c7
root <aleenamaryam@gmail.com>
2019-06-11 18:40:56 +0000

    js edits

--- newsite/static/letters.js
+++ newsite/static/letters.js
@@ -1,29 +1,42 @@
 
+mapboxgl.accessToken  = 'pk.eyJ1IjoiYW1hcnlhbSIsImEiOiJjandqY2kxMTgwajRyNDlwN2N0MzJpd2FmIn0.BYTMqLbeeAG6YGSJjS1gZg';
 
-mapboxgl.accessToken  = 'pk.eyJ1IjoiYW1hcnlhbSIsImEiOiJjandqY2phNzkwaGtyNDhtcjVjYm0yd2\
-pmIn0.DWx8N5QUgoa9EezgnZmNbQ';
 var mappie = new mapboxgl.Map({
     container: 'mapdiv',
     style: 'mapbox://styles/amaryam/cjwjclh340t991cpazyi2ruba',
-    center: [ -75.317630,40.007270],
-    zoom: 5
+    center: [ -75.317630, 40.007270],
+    zoom: 4
 });
 
 let data = {};
-function msg(){
-    alert("Hello!");
-}
 
 function preload() {
     data = loadJSON(url);
    
 }
-console.log(data);
+
 function setup() {
     createCanvas(640, 360);
     loadData();
 }
+
 function loadData() {
     console.log(data[0]);
 }
 
+// reference Daniel Shiffman, Visualizing Earthquakes with p5
+// Web Mercator formulas
+function mercX(lon) {
+    lon = radians(lon);
+    var a = (512/(2*PI)) * pow(2,zoom);
+    var b = lon + PI;
+    return a * b;
+}
+
+function mercY(lat) {
+    lat = radians(lat);
+    var a = (512/(2*PI)) * pow(2,zoom);
+    var b = tan(PI/4 + lat/2);
+    var c = PI - log(b); // default is ln()
+    return a * c;
+}
