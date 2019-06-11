e2e26697b5487196f48090c5177e20b04b6323d7
a-maryam <aleenamaryam@gmail.com>
2019-06-11 13:44:38 +0000

    basic map with external js

--- newsite/static/letters.js
+++ newsite/static/letters.js
@@ -1,4 +1,14 @@
-//console.log(url);
+
+
+mapboxgl.accessToken  = 'pk.eyJ1IjoiYW1hcnlhbSIsImEiOiJjandqY2phNzkwaGtyNDhtcjVjYm0yd2\
+pmIn0.DWx8N5QUgoa9EezgnZmNbQ';
+var mappie = new mapboxgl.Map({
+    container: 'mapdiv',
+    style: 'mapbox://styles/amaryam/cjwjclh340t991cpazyi2ruba',
+    center: [ -75.317630,40.007270],
+    zoom: 5
+});
+
 let data = {};
 function msg(){
     alert("Hello!");
