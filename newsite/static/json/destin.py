43c986d209d80ac95e25101731bcfa87755a81eb
root <aleenamaryam@gmail.com>
2019-06-14 15:09:28 +0000

    fixed destin coords

--- newsite/static/json/destin.py
+++ newsite/static/json/destin.py
@@ -3,7 +3,8 @@ import json
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key = 'AIzaSyCcgY4MwelgM_TGBsyELyHxkDQ30GNnWAM')
+#gmaps = googlemaps.Client(key = 'AIzaSyCcgY4MwelgM_TGBsyELyHxkDQ30GNnWAM')
+# first do place coordinates
 letters = open('letters.json','r')
 dict = json.load(letters)
 with open('destin_coords.json', 'w') as f:
@@ -11,10 +12,27 @@ with open('destin_coords.json', 'w') as f:
     for elem in dict:
         x = json.dumps(elem['destin'])
         if x!='{}':
-            geocode_result = gmaps.geocode(x) # geocode_result is a list
-            for item in geocode_result:
-                f.write("%s" % item)
+#            geocode_result = gmaps.geocode(x) # geocode_result is a list
+#            for item in geocode_result:
+#                f.write("%s" % item)
+            print("Blah blah")
             f.write(",")
         else:
             f.write("{},")
     f.write("]")
+
+# did not use this code:
+# now do destin coordinates
+with open('letters.json', 'r') as f:
+    dict = json.load(f)
+    f = open('destin_coords.json', 'w')
+    for elem in dict:
+        x = json.dumps(elem['place'])
+        if x!='{}':
+            geocode_result = gmaps.geocode(x) # geocode_result is a list
+            for item in geocode_result:
+                f.write("%s" % item)
+                f.write(",")
+        else:
+            f.write("{},")
+        f.write("]")
