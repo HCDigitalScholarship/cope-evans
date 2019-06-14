4132669b1afd36a9e1a28e09274ae6f8f6b6aeb0
root <aleenamaryam@gmail.com>
2019-06-14 20:29:07 +0000

    in process of combining json

--- newsite/static/json/load_place.py
+++ newsite/static/json/load_place.py
@@ -1,3 +1,30 @@
-# purpose: load place data into the letter.json file.
+# purpose: load place data into the letters.json file.
 # initially will test by loading into test file
+# accessing elements in the json dictionary letters.json
+# writing to the file letters...
+# load data into python object, edit, then write to new file
+
+import json
+
+f =open('letters.json','r')
+dict = json.load(f)
+more_json = open('place_coords.json','r')
+places = json.load(more_json)
+with open('update_letters.json','w') as letters:
+    print(type(places))
+    for elem in dict:
+        for place in places:
+            try:
+                elem.update({'place-coords' : place['geometry']['location']})
+                print(elem)
+                #break
+            except KeyError:
+                elem.update({'place-coords' : {}})
+            except TypeError:
+                elem.update({'place-coords' : {}})
+        for item in elem:
+            letters.write("%s" % item)
+        break
+        letters.write(",")
+    letters.write("]")
 
