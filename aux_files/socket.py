ed73ce48a7e0bca19a92b6c1c2fc4c3c41b3fa18
Aleena Maryam <40567078+a-maryam@users.noreply.github.com>
2019-06-16 09:38:24 -0400

    added socket.py for contentdm queries

---
+++ aux_files/socket.py
@@ -0,0 +1,11 @@
+#actually used this
+import requests
+f = open('pointers.txt','r')
+out = open("letters.json","w+")
+for word in f:
+    for line in f:
+        for word in line.split():
+            cmd = 'http://165.106.6.76:81/dmwebservices/index.php?q=dmGetItemInfo/cope/' + word + '/json'
+            response = requests.get(cmd)
+            out.write(response.text + ',\n')
+# added opening and closing brackets to the json file myself, also remove final ','
