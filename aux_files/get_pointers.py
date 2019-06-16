c636fb0eb9b9fcd0d7e374757ce678ba0c944e1b
root <aleenamaryam@gmail.com>
2019-06-16 14:53:48 +0000

    updated pointers.txt

--- aux_files/get_pointers.py
+++ aux_files/get_pointers.py
@@ -6,7 +6,7 @@ f = open('collection_info.json','r')
 dict = json.load(f)
 # dict[0]['records'][1]['pointer']
 records = dict[0]['records']
-
+# print(len(records))
 with open('pointers.txt','w', encoding = 'utf-8') as out:
     for item in records:
         print(item)
