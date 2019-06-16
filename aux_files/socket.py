72b5eacf2c3a4a55811507d45f633a4348d64e72
root <aleenamaryam@gmail.com>
2019-06-16 14:02:45 +0000

    editing socket.py, problem with requests on run

--- aux_files/socket.py
+++ aux_files/socket.py
@@ -1,11 +1,14 @@
 #actually used this
 import requests
+#from requests import *
 f = open('pointers.txt','r')
 out = open("letters.json","w+")
+out.write("[")
 for word in f:
     for line in f:
         for word in line.split():
             cmd = 'http://165.106.6.76:81/dmwebservices/index.php?q=dmGetItemInfo/cope/' + word + '/json'
             response = requests.get(cmd)
             out.write(response.text + ',\n')
+out.write("]") # update: now only need to worry about final ','
 # added opening and closing brackets to the json file myself, also remove final ','
