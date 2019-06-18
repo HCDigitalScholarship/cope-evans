62339e650b64aec881b70f44094f2e15aef9d1a3
root <aleenamaryam@gmail.com>
2019-06-18 13:33:47 +0000

    got destination coords

--- newsite/static/json/destin_geocode.py
+++ newsite/static/json/destin_geocode.py
@@ -4,13 +4,13 @@ import json
 import requests
 import os
 
-with open('letters.json', 'r') as f:
-    dict  = json.load(f) # pretty sure it will act as a dictionary
+with open('letters.json', 'r') as letters:
+    dict  = json.load(letters) # pretty sure it will act as a dictionary
 
-f = open('coordinates.json', 'w', encoding = 'utf-8')
+f = open('destin_coordinates.json', 'w', encoding = 'utf-8')
 f.write('[')
 for elem in dict:
-    x = json.dumps(elem['place'])
+    x = json.dumps(elem['destin'])
     if x!='{}':
         url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ x + ".json?access_token=pk.eyJ1IjoiYW1hcnlhbSIsImEiOiJjandqY2kxMTgwajRyNDlwN2N0MzJpd2FmIn0.BYTMqLbeeAG6YGSJjS1gZg"
         r = requests.get(url)
