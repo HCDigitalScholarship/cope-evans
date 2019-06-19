6d1f9e60ee47724043f44b30571876896d8c0ed5
root <aleenamaryam@gmail.com>
2019-06-19 17:39:31 +0000

    trying to get js to work, commit to preserve comments

--- newsite/static/letters.js
+++ newsite/static/letters.js
@@ -1,4 +1,4 @@
-
+/*
 mapboxgl.accessToken  = 'pk.eyJ1IjoiYW1hcnlhbSIsImEiOiJjandqY2kxMTgwajRyNDlwN2N0MzJpd2FmIn0.BYTMqLbeeAG6YGSJjS1gZg';
 
 var mappie = new mapboxgl.Map({
@@ -6,29 +6,33 @@ var mappie = new mapboxgl.Map({
     style: 'mapbox://styles/amaryam/cjwjclh340t991cpazyi2ruba',
     center: [ -75.317630, 40.007270],
     zoom: 4
-});
+});*/
+// creating a new array probably isn't the best way to do this, but I just want to see if this method works
+// Note: I probably do not have to load the whole JSON file for simpler visualizations. I do if I want the other metadata.
 
-let data = {};
-
+var letters = [];
+var place_points = [];
+console.log(place_points);
 function preload() {
-    data = loadJSON(url);
-    letters = loadJSON(letters_url);
-    // just use a relative path (there was a problem with that -- but actually I had already found a 'hacky' solution)
-//    hmmm = loadJSON('/geocoding/v5/mapbox.places/haverford.json');
+    letters = loadJSON(letters_url);    
 }
 
-function setup() {
-    //createCanvas(640, 360);
-    // how is the image centered
-    loadData();
+function setup() {    
+    var i;
+    for (i = 0; i < letters.length; i++) {
+	console.log("Hellooo");
+	console.log(letters[i]['place-coordinates']);
+	//place_points.push(letters[i]['place-coordinates']);
+	//console.log("hi");
+	//console.log(place_points[i]);
+	break;
+    }
+    //loadData();
 }
 
-function loadData() {
-    console.log(data[0]);
-    console.log(letters[0]);
-  //  console.log(hmmm);
-}
-// this will only work in static situations...maybe use for animation
+/*function loadData() {
+    console.log(letters[0]['place-coordinates']);
+}*/
 // reference Daniel Shiffman, Visualizing Earthquakes with p5
 // Web Mercator formulas
 function mercX(lon) {
