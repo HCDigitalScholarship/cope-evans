8a552db8421cb178db79cd141d32198398beacfb
Aleena Maryam <40567078+a-maryam@users.noreply.github.com>
2019-06-27 15:42:30 -0400

    Update inspect_mapbox_json.py

--- aux_files/create_letters_json/inspect_mapbox_json.py
+++ aux_files/create_letters_json/inspect_mapbox_json.py
@@ -1,7 +1,5 @@
+# I was just experimenting and trying to figure out how the mapbox JSON was formatted
 import json
-
 f = open('place_coordinates.json','r')
-
 dict = json.load(f)
-
 print(dict[255]['features'][0]['geometry']['coordinates'])
