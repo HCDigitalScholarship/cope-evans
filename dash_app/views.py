1ace38420aa27bb366d3b944cf7f6829bd48eeb5
root <aleenamaryam@gmail.com>
2019-06-28 13:21:32 +0000

    app config changes

--- dash_app/views.py
+++ dash_app/views.py
@@ -1,3 +1,7 @@
+
 from django.shortcuts import render
+import Django_Dash_app.dashplotly.dashboard_app
 
 # Create your views here.
+def text_analysis(request):
+        return render(request, "morris_app.html")
