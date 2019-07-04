356c8f62f5bd10db6ada3ea469f17bd5c3b42fec
root <aleenamaryam@gmail.com>
2019-07-04 22:14:58 +0000

    subject headings are being graphed, will rm labels and choose top 10

--- newsite/static/js/text.js
+++ newsite/static/js/text.js
@@ -13,42 +13,39 @@ function setup() {
     console.log(morris_data[0]);
  
     for(var i=0; i < length; i++) {
-	// handle key error
-    // split by semicolon
-        if (!isEmpty(morris_data[i]['subjec'])) {
-
-	try {
-        spl = morris_data[0]['subjec'].split(";");
-        for(var v = 0; v < spl.length; v++) {
-            if (!words.has(spl[i])) {
-                words.set(spl[i], 1);
-            }
-            else {
-                words.set(spl[i], words.get(spl[i]) + 1);
-            }
-        }
+        if (!isEmpty(morris_data[i])) {
+	    try {
+		console.log("hi");
+		spl = morris_data[i]['subjec'].split(";");
+		for(var v = 0; v < spl.length; v++) {
+		    if (!words.has(spl[v])) {
+			words.set(spl[v], 1);
+			console.log("Made it");
+		    }
+		    else {
+			words.set(spl[v], words.get(spl[v]) + 1);
+		    }
+		}
+	    }
+	    catch(TypeError) {
+		continue;
+	    }
 	}
-	catch(TypeError) {
-	    continue;
     }
-}
-    else {
-        continue;
-    }
-}
 
     console.log(words);
-
-    /*for(var j = 0; j < length; j++) {
+    var iterator1 = words.keys();
+    for(var j = 0; j < 530; j++) {
 	try {
-	    actual_words.push(morris_data[i]['subjec']);
-	    occurrences.push(words.get(morris_data[i]['subjec']));
+	    actual_words.push(iterator1.next().value);
+	    occurrences.push(words.get(iterator1.next().value));
 	}
 	catch {
 	    continue;
 	}
-    }*/
-    //console.log(actual_words);
+    }
+    
+    console.log(actual_words);
 
     new Chart(document.getElementById("bar-chart"), {
 	type: 'bar',
@@ -70,11 +67,12 @@ function setup() {
       }
     }
     });
-
+}
 
     function isEmpty(obj) {
-        if (obj.hasOwnProperty('subjec') && errors['subjec']) {
-		return false;
+        if (obj.hasOwnProperty('subjec') && obj['subjec']) {
+	    console.log('success');
+	    return false;
 	    }
 	    return true;
     }
