6697d1bf02de6d3d8df1d63399533235ea50ddcc
root <aleenamaryam@gmail.com>
2019-07-04 19:15:40 +0000

    js

--- newsite/static/js/text.js
+++ newsite/static/js/text.js
@@ -4,27 +4,55 @@ morris_data = [];
 //console.log(morris_data[0]);
 var words = new Map();
 var length = 624;
+var actual_words = []; // will contain 
+var occurrences = [];
 function preload() {
     morris_data = loadJSON(morris_url);
 }
 function setup() {
     console.log(morris_data[0]);
     //console.log(morris_data.length);
+//    var temp;
     for(var i=0; i < length; i++) {
-	if(!words.has(morris_data[i]['subjec'])) {
-	    words.set(morris_data[i]['subjec'], 1);
+	// handle key error
+	// split by semicolon
+	try {
+	    
+	    if(!words.has(morris_data[i]['subjec'])) {
+		words.set(morris_data[i]['subjec'], 1);
+	    }
+	    else if (!isEmpty(morris_data[i]['subjec'])) {
+		words.set(morris_data[i]['subjec'], words.get(morris_data[i]['subjec'])+1);
+	    }
 	}
-	else {
+	catch(TypeError) {
+	    continue;
 	}
+	}
+    }
+	
+    console.log(words);
+
+    for(var j = 0; j < length; j++) {
+	try {
+	    actual_words.push(morris_data[i]['subjec']);
+	    occurrences.push(words.get(morris_data[i]['subjec']));
+	}
+	catch {
+	    continue;
+	}
+    }
+    console.log(actual_words);
+	
     new Chart(document.getElementById("bar-chart"), {
 	type: 'bar',
     data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+	labels: actual_words,
       datasets: [
         {
           label: "Number of appearances",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          //backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: occurrences,
         }
       ]
     },
@@ -35,6 +63,13 @@ function setup() {
         text: 'Subject tags in J. Morris Evans documents (mainly letters)'
       }
     }
-});
+    });
 
-}
+
+    function isEmpty(obj) {
+	for(var key in obj) {
+	    if(obj.hasOwnProperty(key))
+		return false;
+	    }
+	    return true;
+    }
