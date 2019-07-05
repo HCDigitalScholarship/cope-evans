870c309b3c6bf84665846799766487f81e128bd3
root <aleenamaryam@gmail.com>
2019-07-05 13:49:22 +0000

    working on chart

--- newsite/static/js/text.js
+++ newsite/static/js/text.js
@@ -15,15 +15,14 @@ function setup() {
     for(var i=0; i < length; i++) {
         if (!isEmpty(morris_data[i])) {
 	    try {
-		console.log("hi");
 		spl = morris_data[i]['subjec'].split(";");
 		for(var v = 0; v < spl.length; v++) {
 		    if (!words.has(spl[v])) {
 			words.set(spl[v], 1);
-			console.log("Made it");
 		    }
 		    else {
 			words.set(spl[v], words.get(spl[v]) + 1);
+			//console.log(words.get(spl[v]));
 		    }
 		}
 	    }
@@ -34,6 +33,7 @@ function setup() {
     }
 
     console.log(words);
+    console.log(words.length);
     var iterator1 = words.keys();
     for(var j = 0; j < 530; j++) {
 	try {
@@ -46,6 +46,9 @@ function setup() {
     }
     
     console.log(actual_words);
+   // var newArray = associativeQsort(actual_words, appearances, 0, 530);
+    //console.log(newArray);
+    
 
     new Chart(document.getElementById("bar-chart"), {
 	type: 'bar',
@@ -71,8 +74,50 @@ function setup() {
 
     function isEmpty(obj) {
         if (obj.hasOwnProperty('subjec') && obj['subjec']) {
-	    console.log('success');
+	    //console.log('success');
 	    return false;
 	    }
 	    return true;
     }
+// will sort wrt to appearances and maintain associativity
+// Apparently there's no way to sort hashmaps wrt to Value.
+function associativeQSort(words1, appearances, left, right) {
+    var length = words1.length;
+    var pivot;
+    var part;
+    
+    if(left < right) {
+	pivot = right;
+	part = partition(words1, appearances, pivot, left, right);
+	associativeQSort(words1, appearances, left, part - 1);
+	associativeQSort(words1, appearances, part + 1, right)
+    }
+    
+    return [
+	words1,
+	appearances
+    ];
+}
+
+function partition(words1, appearances, pivot, left, right) {
+    var pivotValue = appearances[pivot];
+    var partitionIndex = left;
+    for(var i = left; i < right; i++) {
+	if(appearances[i] < pivotValue) {
+	    swap(words1, appearances, i, partitionIndex);
+	    partitionIndex++;
+	}
+    }
+    swap(words1, appearances, right, partitionIndex);
+    return partitionIndex;
+}
+
+
+function swap(words1, appearances, i, j) {
+    var numTemp = appearances[i];
+    var wordTemp = words1[i];
+    appearances[i] = appearances[j];
+    appearances[j] = numTemp;
+    words1[i] = words1[j];
+    words1[j] = wordTemp;
+}
