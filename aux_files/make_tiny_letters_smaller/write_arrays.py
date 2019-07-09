6551de60d3a49387c0835c4b52215415460815c3
root <aleenamaryam@gmail.com>
2019-07-09 19:33:00 +0000

    regenerated mappable_items.json

--- aux_files/make_tiny_letters_smaller/write_arrays.py
+++ aux_files/make_tiny_letters_smaller/write_arrays.py
@@ -6,13 +6,14 @@ def main():
     dict = json.load(f)
     data = ObjDict()
     file = open('mappable_items.json', 'w', encoding = 'utf-8')
+    print(len(dict))
     origins = []
     destinations = []
     for i in range(len(dict)):
-        if isEmpty(dict[i]['place'])==False:
+        if isEmpty(dict[i]['place'])==False or isEmpty(dict[i]['place-coordinates'])==False:
            origins.append(dict[i])
     for i in range(len(dict)):
-        if isEmpty(dict[i]['destin']) == False:
+        if isEmpty(dict[i]['destin']) == False or isEmpty(dict[i]['destin-coordinates']) == False:
             destinations.append(dict[i]);
     print(len(origins))
     print(len(destinations))
@@ -22,7 +23,7 @@ def main():
     json.dump(data, file)
     
 def isEmpty(obj):
-    if obj:
+    if obj and  obj!= '' and len(obj)!=0:
         return True
     else:
         return False
